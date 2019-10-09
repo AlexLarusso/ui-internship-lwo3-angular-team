278466b82a8039c98ba6eb71d7932ba54fe055f9
vantonov <vantonov@griddynamics.com>
2019-10-09 16:14:54 +0300

    Scroll anchor directive added

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -22,6 +22,7 @@ import { MainMenuComponent } from './header/main-menu/main-menu.component';
 import { ParallaxComponent } from './parallax/parallax.component';
 import { PopularListComponent } from './popular-list/popular-list.component';
 import { ProductShortInfoService } from './product-short-info.service';
+import { ScrollAnchorDirective } from './scroll-anchor.directive';
 
 @NgModule({
   declarations: [
@@ -40,7 +41,8 @@ import { ProductShortInfoService } from './product-short-info.service';
     ProductListComponent,
     MainMenuComponent,
     ParallaxComponent,
-    PopularListComponent
+    PopularListComponent,
+    ScrollAnchorDirective
   ],
   imports: [
     BrowserModule,
