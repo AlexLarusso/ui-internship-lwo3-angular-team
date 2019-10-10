76512af2ab2de81b1b555094c1d113eab4d94c70
TorskyM <mikhailotorsky@gmail.com>
2019-10-10 12:26:57 +0300

    Pages added

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,18 +1,18 @@
 import { NgModule }             from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 
-import { NotFoundComponent }    from './not-found/not-found.component';
-import { PopularListComponent } from './popular-list/popular-list.component';
-import { ProductItemComponent } from './shared/product-item/product-item.component';
-import { ProductListComponent } from './product-list/product-list.component';
+import { NotFoundComponent }        from './pages/not-found/not-found.component';
+import { HomeComponent }            from './pages/home/home.component';
+import { ProductListPageComponent } from './pages/product-list-page/product-list-page.component';
+import { ProductDetailsPageComponent } from './pages/product-details-page/product-details-page.component';
 
 const routes: Routes = [
   { path: '', redirectTo: '/home', pathMatch: 'full' },
-  // { path: '**', redirectTo: '/404'},
-  { path: '404', component: NotFoundComponent, data: { title: 'Heroes List' }},
-  { path: 'products', component: ProductListComponent, data: { title: 'Product List' } },
-  { path: 'products/:id', component: ProductItemComponent, data: { title: 'Product Item' } },
-  { path: 'home', component: PopularListComponent, data: { title: 'Popular' } }
+  { path: 'home',         component: HomeComponent, data: { title: 'Home' } },
+  { path: 'products',     component: ProductListPageComponent, data: { title: 'Product List' } },
+  { path: 'products/:id', component: ProductDetailsPageComponent, data: { title: 'Product Item' } },
+  { path: '404',          component: NotFoundComponent, data: { title: 'Not Found' }},
+  { path: '**',           redirectTo: '/404'},
 ]
 
 @NgModule({
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,60 +1,72 @@
-import { BrowserModule } from '@angular/platform-browser';
-import { NgModule } from '@angular/core';
+import { BrowserModule }         from '@angular/platform-browser';
+import { NgModule }              from '@angular/core';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
-import { HttpClientModule } from '@angular/common/http';
-import { HttpService } from './http.service';
-import { FontAwesomeModule } from '@fortawesome/angular-fontawesome';
+import { HttpClientModule }      from '@angular/common/http';
+import { HttpService }           from './http.service';
+import { FontAwesomeModule }     from '@fortawesome/angular-fontawesome';
 
-import { AppComponent } from './app.component';
-import { HeaderComponent } from './header/header.component';
-import { ButtonComponent } from './shared/button/button.component';
-import { MainLogoComponent } from './shared/main-logo/main-logo.component';
-import { ProductItemComponent } from './shared/product-item/product-item.component';
-import { PromoComponent } from './header/promo/promo.component';
+import { AppComponent }          from './app.component';
+import { HeaderComponent }       from './header/header.component';
+import { ButtonComponent }       from './shared/button/button.component';
+import { MainLogoComponent }     from './shared/main-logo/main-logo.component';
+import { ProductItemComponent }  from './shared/product-item/product-item.component';
+import { PromoComponent }        from './promo/promo.component';
 import { CategoryMenuComponent } from './header/category-menu/category-menu.component';
-import { LineBarComponent } from './header/line-bar/line-bar.component';
-import { FooterComponent } from './footer/footer.component';
-import { SaleBannerComponent } from './shared/sale-banner/sale-banner.component';
-import { MainComponent } from './main/main.component';
-import { StoryComponent } from './main/story/story.component';
-import { ProductListComponent } from './product-list/product-list.component';
-import { MainMenuComponent } from './header/main-menu/main-menu.component';
-import { ParallaxComponent } from './parallax/parallax.component';
-import { PopularListComponent } from './popular-list/popular-list.component';
+import { LineBarComponent }      from './header/line-bar/line-bar.component';
+import { FooterComponent }       from './footer/footer.component';
+import { SaleBannerComponent }   from './shared/sale-banner/sale-banner.component';
+import { MainComponent }         from './main/main.component';
+import { StoryComponent }        from './main/story/story.component';
+import { ProductListComponent }  from './product-list/product-list.component';
+import { MainMenuComponent }     from './header/main-menu/main-menu.component';
+import { ParallaxComponent }     from './parallax/parallax.component';
+import { PopularListComponent }  from './popular-list/popular-list.component';
 import { ProductShortInfoService } from './product-short-info.service';
-import { NotFoundComponent } from './not-found/not-found.component';
-import { AppRoutingModule } from './app-routing.module';
+import { NotFoundComponent }     from './pages/not-found/not-found.component';
+import { AppRoutingModule }      from './app-routing.module';
+import { HomeComponent }         from './pages/home/home.component';
+import { ProductListPageComponent } from './pages/product-list-page/product-list-page.component';
+import { ProductDetailsComponent }  from './product-details/product-details.component';
+import { ProductDetailsPageComponent } from './pages/product-details-page/product-details-page.component';
 
+const IMPORTS = [
+  BrowserModule,
+  BrowserAnimationsModule,
+  HttpClientModule,
+  FontAwesomeModule,
+  AppRoutingModule];
+
+const DECLARATIONS = [
+  AppComponent,
+  HeaderComponent,
+  ButtonComponent,
+  MainLogoComponent,
+  ProductItemComponent,
+  PromoComponent,
+  CategoryMenuComponent,
+  LineBarComponent,
+  FooterComponent,
+  SaleBannerComponent,
+  MainComponent,
+  StoryComponent,
+  ProductListComponent,
+  MainMenuComponent,
+  ParallaxComponent,
+  PopularListComponent,
+  NotFoundComponent,
+  HomeComponent,
+  ProductListPageComponent,
+  ProductDetailsComponent,
+  ProductDetailsPageComponent];
+
+  const PROVIDERS = [
+    HttpService,
+    ProductShortInfoService];
 
 @NgModule({
-  declarations: [
-    AppComponent,
-    HeaderComponent,
-    ButtonComponent,
-    MainLogoComponent,
-    ProductItemComponent,
-    PromoComponent,
-    CategoryMenuComponent,
-    LineBarComponent,
-    FooterComponent,
-    SaleBannerComponent,
-    MainComponent,
-    StoryComponent,
-    ProductListComponent,
-    MainMenuComponent,
-    ParallaxComponent,
-    PopularListComponent,
-    NotFoundComponent,
-  ],
-  imports: [
-    BrowserModule,
-    BrowserAnimationsModule,
-    HttpClientModule,
-    FontAwesomeModule,
-    AppRoutingModule
-  ],
-  providers: [HttpService,
-    ProductShortInfoService],
+  declarations: [...DECLARATIONS],
+  imports: [...IMPORTS],
+  providers: [...PROVIDERS],
   bootstrap: [AppComponent]
 })
 export class AppModule {
