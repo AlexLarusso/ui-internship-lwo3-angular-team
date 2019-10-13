0d5169a1f1a85eae6ce0fe0df49208ef72bc6248
castan1c <13decoy@gmail.com>
2019-10-13 06:13:28 +0300

    Fixed the tests

--- src/app/scroll/scroll.component.spec.ts
+++ src/app/scroll/scroll.component.spec.ts
@@ -1,14 +1,10 @@
 import { async, ComponentFixture, TestBed } from '@angular/core/testing';
 import { FontAwesomeModule } from '@fortawesome/angular-fontawesome';
 import { ScrollComponent } from './scroll.component';
-import { ScrollService } from '../scroll.service';
-import { ElementRef } from '@angular/core';
-
 
 describe('ScrollComponent', () => {
   let component: ScrollComponent;
   let fixture: ComponentFixture<ScrollComponent>;
-  let service: ScrollService;
   const ITEMS_NUMBER = 10;
 
   beforeEach(async(() => {
@@ -19,12 +15,14 @@ describe('ScrollComponent', () => {
   }));
 
   beforeEach(() => {
-    service = TestBed.get(ScrollService);
-    for(let i = 0; i < ITEMS_NUMBER; i++) {
-      service.addAnchor(new ElementRef('div'));
-    }
     fixture = TestBed.createComponent(ScrollComponent);
     component = fixture.componentInstance;
+    for(let i = 0; i < ITEMS_NUMBER; i++) {
+      component.pageComponents.push({
+        selector: i.toString(),
+        title: i.toString()
+      });
+    }
     fixture.detectChanges();
   });
 
@@ -32,29 +30,20 @@ describe('ScrollComponent', () => {
     expect(component).toBeTruthy();
   });
 
+  it('should render the correct number of items', () => {
+    const items = [...fixture.debugElement.nativeElement.querySelectorAll('li')];
+    
+    expect(items.length).toEqual(ITEMS_NUMBER);
+  });
+
   it('Item clicks should call onSelect method', () => {
     const fakeClick = new MouseEvent('click', {bubbles: true});
     const items = [...fixture.debugElement.nativeElement.querySelectorAll('li')];
     const spy = spyOn(component, 'onItemSelect');
-
+  
     items.forEach(item => {
       item.dispatchEvent(fakeClick);
     });
     expect(spy).toHaveBeenCalledTimes(items.length);
   });
-
-  it('Should have number of items as provided by service', () => {
-    expect(component.pageComponents.length).toEqual(ITEMS_NUMBER);
-  });
-
-  it('Should update number of items', () => {
-    const additionalItems = 5;
-    for(let i = 0; i < additionalItems; i++) {
-      service.addAnchor(new ElementRef('div'));
-    }
-    expect(component.pageComponents.length).toEqual(ITEMS_NUMBER + additionalItems);
-  });
-
-
-
 });
