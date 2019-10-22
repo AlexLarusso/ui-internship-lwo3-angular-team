ca5cc1485abb95b4b337c957d1ee5845540ee6de
castan1c <13decoy@gmail.com>
2019-10-22 22:53:59 +0300

    Applied rxjs filter

--- src/app/shared/services/product.service.ts
+++ src/app/shared/services/product.service.ts
@@ -1,6 +1,6 @@
 import { Injectable } from '@angular/core';
 import { Observable } from 'rxjs';
-import { map } from 'rxjs/operators';
+import { switchMap, find } from 'rxjs/operators';
 
 import { IProduct } from '../../interfaces/product.interface';
 import { HttpService } from './http.service';
@@ -12,8 +12,10 @@ export class ProductService {
   constructor(private httpService: HttpService) { }
 
   public getProduct(id: string | number): Observable<IProduct> {
-    return this.httpService.getData().pipe(map(products =>
-      products.find(product => product.id === id)
-    ));
+    return this.httpService.getData()
+      .pipe(
+        switchMap(data => data),
+        find(product => product.id === id)
+    );
   }
 }
