1afb1d24a696e2d17f2fe77c63f405d28ad17d0b
TorskyM <mikhailotorsky@gmail.com>
2019-11-01 17:00:32 +0200

    layout fix

--- src/app/shared/directives/image-loader.directive.ts
+++ src/app/shared/directives/image-loader.directive.ts
@@ -12,15 +12,22 @@ import {
 export class ImageLoadDirective implements AfterViewInit {
   @Output() public downloadImage = new EventEmitter();
 
-  private observer?: IntersectionObserver;
+  public options = {
+    root: document.querySelector('.root'),
+    rootMargin: '0px',
+    threshold: [0, 0.5, 1.0]
+};
+
+  private observer: IntersectionObserver;
 
   constructor(private element: ElementRef) { }
 
   public ngAfterViewInit(): void {
-    this.observer = new IntersectionObserver(entries => {
-      this.checkForIntersection(entries);
-    }, { });
-    this.observer.observe((this.element.nativeElement));
+    this.observer = new IntersectionObserver(([entries]) => {
+      this.checkForIntersection([entries]);
+    }, this.options);
+
+    this.observer.observe(this.element.nativeElement);
   }
 
   private checkIntersecting(entrance: IntersectionObserverEntry) {
@@ -32,7 +39,7 @@ export class ImageLoadDirective implements AfterViewInit {
     entries.every((entrance: IntersectionObserverEntry) => {
       if (this.checkIntersecting(entrance)) {
         this.downloadImage.emit();
-        this.observer.unobserve((this.element.nativeElement));
+        this.observer.unobserve(this.element.nativeElement);
         this.observer.disconnect();
       }
     });
